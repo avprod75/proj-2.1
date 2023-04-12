a6b0aab20f411600510e6e77e905fed03bc1f0a5
avprod75 <andrio2019voyt@gmail.com>
2023-04-12 11:02:43 +0300

    init commit

--- main.cpp
+++ main.cpp
@@ -1,39 +1,19 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-int main()
-{
-    int x0, y0, r1, r2, x, y,R1,R2;
-    float L;
-    cout << "Input coordinates of circle's center (X0, Y0): ";
-    cin >> x0 >> y0;
-    cout << "Input circle radiuses R1 and R2: ";
-    cin >> r1 >> r2;
-    if(r1<0)
-    {
-        cout<<"Erorrrrrrr1";
-        return 0;
+
+int main() {
+
+    int n, range;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    cout << "Enter range: ";
+    cin >> range;
+    
+    for (int i = 1; i <= range; ++i) {
+        cout << n << " * " << i << " = " << n * i << endl;
     }
-    if(r2<0)
-    {
-        cout<<"Erorrrrrrr2";
-        return 0;
-    }
-    if(r1>r2)
-    {
-        cout<<"Erorrrrrrr3";
-        return 0;
-    }
-    cout << "Input point coordinates (X, Y): ";
-    cin >> x >> y;
-    L = sqrt(pow(x - x0, 2) + pow(y - y0, 2));
-    if ((r1 < L) && (L < r2))
-    {
-        cout << "This point is situated inside the circle.\n";
-    }
-    else
-    {
-        cout << "This point is not situated inside the circle.\n";
-    }
+    
     return 0;
 }
